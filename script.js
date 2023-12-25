740712dd845a85836b2fed3ddbcddaae35b6bd95
Sparsh Chauhan <142965846+Sparsh261@users.noreply.github.com>
2023-12-25 22:19:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+     
+// function show(e){
+//            if(e == 'sk'){
+//             let elem =  document.getElementsByClassName(e)
+//            let flag = 0;
+//            for(let i = 0; i < elem[0].classList.length; i++){
+//                 if(elem[0].classList[i] == 'skills'){
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+//             if(flag == 0){
+//                 for(let i = 0; i < elem.length; i++){
+//                     elem[i].classList.add("skills")
+//                 }
+//             }
+//             else{
+//                 for(let i = 0; i < elem.length; i++){
+//                     elem[i].classList.remove("skills")
+//                 }
+//             }
+//            }
+//            else{
+//             let elem =  document.getElementsByClassName(e)
+//            let flag = 0;
+//            for(let i = 0; i < elem[0].classList.length; i++){
+//                 if(elem[0].classList[i] == 'education'){
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+//             if(flag == 0){
+//                 for(let i = 0; i < elem.length; i++){
+//                     elem[i].classList.add("education")
+//                 }
+//             }
+//             else{
+//                 for(let i = 0; i < elem.length; i++){
+//                     elem[i].classList.remove("education")
+//                 }
+//             }
+//            }
+        
+//         }
+
+
+    
